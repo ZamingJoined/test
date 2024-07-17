04ec9b913db7f91d86df8c5afa90791e3a79e6ca
garajAbel <140595742+garajAbel@users.noreply.github.com>
2024-07-17 23:26:20 +0500

    main
    главные изменения

--- backend/main/main.controller.js
+++ backend/main/main.controller.js
@@ -1,15 +1,16 @@
 const express = require("express")
 const router = express.Router()
 const userService = require("../user/user.service")
+const newsModel = require("../news/news.model")
 
 router.get("/",(req,res)=>{
     const cookie = req.cookies.userCookie
+    const newses = newsModel.getNews()
     if(cookie){
-        
         const user = req.session.user
         if(user){
             if(user.id === cookie.id){
-                res.render("./main/registered",{cookie:cookie})
+                res.render("./main/registered",{cookie:cookie,news:newses})
             }
             else{
                 res.cookie("userCookie",null)
